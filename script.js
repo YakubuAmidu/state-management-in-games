1d0be6a44f8063564ddd16a1840cacef49e7b068
Yakubuamidu <yakubuamidu577@gmail.com>
2023-07-05 08:51:18 -0700

    Initial commit

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ window.addEventListener('load', function(){
 
     function animate(){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        player.draw(ctx);
         player.update(input.lastKey);
+        player.draw(ctx);
         drawStatusText(ctx, input, player);
         requestAnimationFrame(animate);
     };
